a231e92fb4f14c4be13cd37e2600e473efc953f4
params19 <paramsrivastava9@gmail.com>
2025-02-04 00:20:48 +0530

    Refactor login page layout and add primary color constant

---
+++ lib/constants/color.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+const primary_blue = Color(0xff0584Fe);
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:livecom/constants/color.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -18,41 +19,58 @@ class _PhoneLoginState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(254, 254, 255, 255),
-      body: Column(
-        children: [
-          Expanded(child: Image.asset("assets/splashscreen_ui.png")),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text("Welcome to Livecom :)",
-                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
-              const Text("Enter your phone number",
-                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.w400)),
-              const SizedBox(height: 20),
-              Form(
-                key: _formKey,
-                child: TextFormField(
-                  controller: _phoneController,
-                  keyboardType: TextInputType.phone,
-                  decoration: InputDecoration(
-                      hintText: "Phone number",
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: BorderSide(color: Colors.black))),
-                ),
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        width: double.infinity,
+        child: Column(
+          children: [
+            Expanded(
+              child: Image.asset(
+                "assets/splashscreen_ui.png",
               ),
-              SizedBox(height: 20),
-              SizedBox(
-                height: 40,
-                width: 200,
-                child: ElevatedButton(
-                  child: Text('Send OTP'),
-                  onPressed: () {},
-                ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text("Welcome to Livecom :)",
+                      style:
+                          TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
+                  const Text("Enter your phone number",
+                      style:
+                          TextStyle(fontSize: 17, fontWeight: FontWeight.w400)),
+                  const SizedBox(height: 20),
+                  Form(
+                    key: _formKey,
+                    child: TextFormField(
+                      controller: _phoneController,
+                      keyboardType: TextInputType.phone,
+                      decoration: InputDecoration(
+                          hintText: "Phone number",
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(color: primary_blue))),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  SizedBox(
+                    height: 40,
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      child: const Text('Send OTP'),
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        backgroundColor: primary_blue,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
