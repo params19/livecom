ff3d66c31086e6c14da1311e60841c83c581969c
params19 <paramsrivastava9@gmail.com>
2025-02-04 12:29:22 +0530

    Add background color and update HomePage layout for improved UI

--- lib/constants/color.dart
+++ lib/constants/color.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 const primary_blue = Color(0xff0584Fe);
+const background_color = Color(0xfff9f9f9);
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:livecom/constants/color.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,7 +12,47 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: background_color,
+      appBar: AppBar(
+        backgroundColor: background_color,
+        scrolledUnderElevation: 0,
+        elevation: 0,
+        title: Text(
+          "Chats",
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 29),
+        ),
+        actions: [
+          CircleAvatar(
+            backgroundColor: background_color,
+            backgroundImage: Image(
+              image: AssetImage("assets/user.png"),
+            ).image,
+          )
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) => ListTile(
+          leading: Stack(children: [
+            CircleAvatar(
+              backgroundColor: background_color,
+              backgroundImage: Image(
+                image: AssetImage("assets/user.png"),
+              ).image,
+            ),
+            Positioned(
+              right: 0,
+              bottom: 0,
+              child: CircleAvatar(
+                radius: 6,
+                backgroundColor: Colors.green,
+              ),
+            )
+          ]),
+          title: Text("Other User"),
+          subtitle: Text("Hey there!"),
+        ),
+      ),
     );
   }
 }
