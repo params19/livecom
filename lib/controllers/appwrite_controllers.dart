0249693ec895b14536094cfb64e17c6dc71d3a31
params19 <paramsrivastava9@gmail.com>
2025-02-05 18:13:28 +0530

    Add phone number management functions in Appwrite controllers

--- lib/controllers/appwrite_controllers.dart
+++ lib/controllers/appwrite_controllers.dart
@@ -1,4 +1,5 @@
 import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/models.dart';
 
 const String db = "67a318110036e2465bea";
 const String collection = "67a31826000e0b271f0b";
@@ -12,7 +13,7 @@ Client client = Client()
 Account account = Account(client);
 Databases database = Databases(client);
 
-Future savePhoneToDB(
+Future<bool> savePhoneToDB(
     {required String phoneNumber, required String userId}) async {
   try {
     await database.createDocument(
@@ -22,7 +23,52 @@ Future savePhoneToDB(
       data: {'phone': phoneNumber, 'userId': userId},
     );
     print("Phone number saved successfully!");
-  } catch (e) {
+    return true;
+  } on AppwriteException catch (e) {
     print("Error: $e");
+    return false;
   }
+}
+
+Future<String> doesPhoneNumberExist(String phoneNumber) async {
+  try {
+    final DocumentList match = await database.listDocuments(
+      databaseId: db,
+      collectionId: collection,
+      queries: [Query.equal("phone", phoneNumber)],
+    );
+    if (match.documents.isNotEmpty) {
+      final Document doc = match.documents.first;
+      if (doc.data['phone'] != null || doc.data['phone'] != "") {
+        return doc.data['userId'];
+      } else {
+        print("Phone number not found!");
+        return "user_not_found";
+      }
+    } else {
+      print("Phone number not found!");
+      return "user_not_found";
+    }
+  } on AppwriteException catch (e) {
+    print("Error on readind DB $e");
+    return "user_not_found";
+  }
+}
+  Future<String> createPhoneSession({required String phoneNumber}) async {
+    try{
+      final userId= await doesPhoneNumberExist(phoneNumber);
+      if(userId=="user_not_found"){
+        final Token data=await account.createPhoneToken(userId: ID.unique(), phone: phoneNumber);
+        savePhoneToDB(phoneNumber: phoneNumber, userId: data.userId);
+        return data.userId;
+    }
+    else{
+        final Token data=await account.createPhoneToken(userId: userId, phone: phoneNumber);
+        return data.userId;
+    }
+    }
+    on AppwriteException catch(e){
+      print("Login error $e");
+      return "error";
+    }
 }
